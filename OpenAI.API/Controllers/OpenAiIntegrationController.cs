cea86e6fce84cae57008a89c55ba6bd2fd2e176c
Erick Goldberg <erick_goldberg@hotmail.com>
2024-02-15 20:45:49 -0300

    Adicionar arquivos de projeto.

---
+++ OpenAI.API/Controllers/OpenAiIntegrationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI.API.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace OpenAI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAiIntegrationController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        public OpenAiIntegrationController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string text, [FromServices] IConfiguration configuration)
+        {
+            var token = ""; // Api Key da Open AI
+
+            _httpClient.DefaultRequestHeaders.Authorization = 
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var model = new ChatGptInpuModel(text);
+
+            var requestBody = JsonSerializer.Serialize(model);
+
+            var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("https://api.openai.com/v1/completions", content);
+
+            var result = await response.Content.ReadFromJsonAsync<ChatGptViewModel>();
+
+            var promptResponse = result?.choices.FirstOrDefault();
+
+            if (promptResponse == null)
+                throw new Exception();
+
+            return Ok(promptResponse.text.Replace("\n", "").Replace("\t", ""));
+        }
+    }
+}
---
+++ OpenAI.API/Models/ChatGptInpuModel.cs
@@ -0,0 +1,19 @@
+﻿namespace OpenAI.API.Models
+{
+    public class ChatGptInpuModel
+    {
+        public ChatGptInpuModel(string prompt)
+        {
+            this.prompt = prompt;
+
+            temperature = 0.2m; // Margem de erro / criatividade
+            max_tokens = 100; // Quantidade de Palavras 
+            model = "text-davinci-003"; // Qual modelo da Open AI vamo usar
+        }
+
+        public string model{ get; set; }
+        public string prompt { get; set; }
+        public int max_tokens { get; set; }
+        public decimal temperature { get; set; }
+    }
+}
